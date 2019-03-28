45586d2d9ffb48034fdcbf535f80a5abc0729d22
Gopiballava Flaherty <gflaherty@uber.com>
2019-03-28 16:33:18 -0400

    Added the other subtree stuff

---
+++ independence/forgiving/sigh/sorrel/lactates/northeaster/punishment_test.py
@@ -0,0 +1,132 @@
+
+import random
+
+def test_foo():
+    assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+
+def test_bar():
+    assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+
+def test_charlie():
+    assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+
+def test_echo():
+    assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+
+
+def test_delta():
+    assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+
+def test_zebra():
+    assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+    if random.randrange(6) > 2:
+        assert(True)
+
+
+
+
--- make_files.py
+++ make_files.py
@@ -5,7 +5,7 @@ import shutil
 import sys
 
 
-PATH_LENGTH = (2, 5)
+PATH_LENGTH = (1, 2)
 FILE_COUNT = (1, 10)
 
 TEST_SOURCE = "random_test.py"
@@ -32,17 +32,22 @@ def generate_source(dest_path):
     print("   Creating file {}".format(dest_path))
     shutil.copy(TEST_SOURCE, dest_path)
 
-def make_random_dir(depth, file_count):
-    new_dir = get_dir(depth)
+def make_random_dir(base_dir="."):
+    depth = random.randint(PATH_LENGTH[0], PATH_LENGTH[1])
+    file_count = random.randint(FILE_COUNT[0], FILE_COUNT[1])
+    new_dir = os.path.join(base_dir, get_dir(depth))
     print("Making directory {}".format(new_dir))
     os.makedirs(new_dir)
     for i in range(file_count):
         generate_source(new_dir)
+    if base_dir == '.' or random.randrange(6) > 2:
+        for i in range(1 + random.randrange(4)):
+            make_random_dir(base_dir=new_dir)
     return new_dir
 
 
 def main(argv):
-    make_random_dir(3, 4)
+    make_random_dir()
 
 
 if __name__ == "__main__":
