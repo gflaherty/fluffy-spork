8433c743c60a00705a091d63b4382e21e4e30697
Gopiballava Flaherty <gopiballava@gmail.com>
2019-06-16 19:17:12 -0400

    WiP

--- show_tell/timsort_test.py
+++ show_tell/timsort_test.py
@@ -1,8 +1,11 @@
 
-from timsort import sort_a
+from timsort import sort_a, timsort
 
 
 def test_sort_a():
     s = sort_a()
     s.foo()
     assert(True)
+
+def test_timsort():
+    timsort([2, 3, 1, 5, 6, 7])
