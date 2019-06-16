353bf02209a14fb08502c740781e4a236a2550bc
Gopiballava Flaherty <gopiballava@gmail.com>
2019-06-16 19:43:29 -0400

    WiP

--- show_tell/timsort_test.py
+++ show_tell/timsort_test.py
@@ -2,6 +2,7 @@
 from timsort import timsort
 from timsort_b import timsort as timsort_b
 from timsort_c import timsort as timsort_c
+from timsort_d import timsort as timsort_d
 
 def test_timsort():
     ret = timsort([2, 3, 1, 5, 6, 7])
@@ -13,4 +14,9 @@ def test_timsort_b():
 
 def test_timsort_c():
     ret = timsort_c([7, 6, 5, 4, 3, 2, 1])
+    assert(ret == sorted(ret))
+
+def test_timsort_d():
+    ret = timsort_d([7, 6, 5, 4, 3, 2, 1])
+#    ret = timsort_c([2, 3, 1, 5, 6, 7])
     assert(ret == sorted(ret))
