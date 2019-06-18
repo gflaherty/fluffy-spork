2ac9f208e1c1706c915d6e04431965768bfc57e0
Gopiballava Flaherty <gopiballava@gmail.com>
2019-06-18 15:52:33 -0400

    Increasing

--- show_tell/timsort_d.py
+++ show_tell/timsort_d.py
@@ -10,22 +10,14 @@ def binary_search(the_array, item, start, end):
             return start + 1
     if start > end:
         return start
-
     mid = int(round((start + end)/ 2))
-
     if the_array[mid] < item:
         return binary_search(the_array, item, mid + 1, end)
-
     elif the_array[mid] > item:
         return binary_search(the_array, item, start, mid - 1)
-
     else:
         return mid
 
-"""
-Insertion sort that timsort uses if the array size is small or if
-the size of the "run" is small
-"""
 def insertion_sort(the_array):
     l = len(the_array)
     for index in range(1, l):
@@ -35,10 +27,6 @@ def insertion_sort(the_array):
     return the_array
 
 def merge(left, right):
-    """Takes two sorted lists and returns a single sorted list by comparing the
-    elements one at a time.
-    [1, 2, 3, 4, 5, 6]
-    """
     if not left:
         return right
     if not right:
@@ -51,36 +39,23 @@ def timsort(the_array):
     runs, sorted_runs = [], []
     length = len(the_array)
     new_run = [the_array[0]]
-
-    # for every i in the range of 1 to length of array
     for i in range(1, length):
-        # if i is at the end of the list
         if i == length - 1:
             new_run.append(the_array[i])
             runs.append(new_run)
             break
-        # if the i'th element of the array is less than the one before it
         if the_array[i] < the_array[i-1]:
-            # if new_run is set to None (NULL)
             if not new_run:
                 runs.append([the_array[i]])
                 new_run.append(the_array[i])
             else:
                 runs.append(new_run)
-#                 new_run = []
                 new_run = [the_array[i]]
-        # else if its equal to or more than
         else:
             new_run.append(the_array[i])
-
-    # for every item in runs, append it using insertion sort
     for item in runs:
         sorted_runs.append(insertion_sort(item))
-    
-    # for every run in sorted_runs, merge them
     sorted_array = []
     for run in sorted_runs:
         sorted_array = merge(sorted_array, run)
-
     return sorted_array
-
