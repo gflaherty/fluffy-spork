7634971db1558b5318e59d7e1451ebc18401a90f
Gopiballava Flaherty <gopiballava@gmail.com>
2019-06-18 15:48:42 -0400

    Increasing

--- show_tell/timsort_test.py
+++ show_tell/timsort_test.py
@@ -5,34 +5,48 @@ from timsort_b import timsort as timsort_b
 from timsort_c import timsort as timsort_c
 from timsort_d import timsort as timsort_d
 
-arr_a = [2, 3, 1, 5, 6, 7]
+arr = [2, 3, 1, 5, 6, 7]
+arr_a = [7, 1, 2, 3, 4, 5]
 arr_b = [1, 2, 3, 5, 6, 7]
 arr_c = [7, 6, 5, 4, 3, 2, 1]
 arr_d = [6, 7, 5, 4, 3, 2, 1]
 
 def test_timsort():
-    ret = timsort(arr_a)
-    assert(ret == sorted(ret))
-    ret = timsort(arr_b)
-    assert(ret == sorted(ret))
-    ret = timsort(arr_c)
-    assert(ret == sorted(ret))
-    ret = timsort(arr_d)
+    ret = timsort(arr)
     assert(ret == sorted(ret))
 
 def test_timsort_a():
+    ret = timsort_a(arr)
+    assert(ret == sorted(ret))
     ret = timsort_a(arr_a)
     assert(ret == sorted(ret))
 
 def test_timsort_b():
+    ret = timsort_b(arr)
+    assert(ret == sorted(ret))
+    ret = timsort_b(arr_a)
+    assert(ret == sorted(ret))
     ret = timsort_b(arr_b)
     assert(ret == sorted(ret))
 
 def test_timsort_c():
+    ret = timsort_c(arr)
+    assert(ret == sorted(ret))
+    ret = timsort_c(arr_a)
+    assert(ret == sorted(ret))
+    ret = timsort_c(arr_b)
+    assert(ret == sorted(ret))
     ret = timsort_c(arr_c)
     assert(ret == sorted(ret))
 
 def test_timsort_d():
+    ret = timsort_d(arr)
+    assert(ret == sorted(ret))
+    ret = timsort_d(arr_a)
+    assert(ret == sorted(ret))
+    ret = timsort_d(arr_b)
+    assert(ret == sorted(ret))
+    ret = timsort_d(arr_c)
+    assert(ret == sorted(ret))
     ret = timsort_d(arr_d)
-#    ret = timsort_c([2, 3, 1, 5, 6, 7])
     assert(ret == sorted(ret))
